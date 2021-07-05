0c2930a595829182708ab1f3440748b408f8d94b
Daniel <duquedk1@gtmail.com>
2021-07-05 18:27:42 -0500

    finalizacion de captura de elementos

---
+++ app.js
@@ -0,0 +1,9 @@
+const btNum=document.getElementsByName('dNum')
+const btOp=document.getElementsByName('dOpe')
+const btIgual=document.getElementsByName('dIgual')[0]
+const btBorrar=document.getElementsByName('dBorrar')[0]
+let result=document.getElementById('resultado')
+//console.log(botonNum)
+//console.log(btIgual)
+//console.log(btBorrar)
+//console.log(result)
