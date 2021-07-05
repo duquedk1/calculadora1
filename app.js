cdd7588082e49e804db549a26b262a580d41162e
Daniel <duquedk1@gtmail.com>
2021-07-05 18:39:30 -0500

    Programacion de botones

--- app.js
+++ app.js
@@ -7,3 +7,27 @@ let result=document.getElementById('resultado')
 //console.log(btIgual)
 //console.log(btBorrar)
 //console.log(result)
+
+btNum.forEach(function(boton){
+    boton.addEventListener('click', function(){
+        agregarNumero(boton.innerText)
+        //alert(boton.innerText)
+    })
+})
+
+btOp.forEach(function(boton){
+    boton.addEventListener('click', function(){
+        selecOperation(boton.innerText)
+        //alert(boton.innerText)
+    })
+})
+
+btIgual.addEventListener('click',function(){
+    calc()
+    actuaDisplay()
+})
+
+btIgual.addEventListener('click',function(){
+    clear()
+    actuaDisplay()
+})
